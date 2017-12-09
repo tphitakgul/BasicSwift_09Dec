66f8ddf0289ee214b525f1449e76b0dcbb6925ea
Boss <t.phitakgul@gmail.com>
2017-12-09 11:00:55 +0700

    test

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+//This is Add Library
+import UIKit
+
+//var str = "Hello, playground"
+
+// create variable String and fix dataType
+var nameString = "str Boss";
+var numberInt = 123;
+var numberDouble = 12.34;
+var status = true;
+
+//Try Change Value
+nameString = "Doramon";
+numberInt = 321;
+numberDouble = 23.32;
+status = false;
+
+
+
+
