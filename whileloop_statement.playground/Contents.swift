29d373862f97662bf19a24b8c546ab92f04dec80
Boss <t.phitakgul@gmail.com>
2017-12-10 11:11:00 +0700

    while loop ok

---
+++ whileloop_statement.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var timesInt = 0
+while (timesInt <= 10) {
+    
+    print(" times --> \(timesInt)")
+    
+    timesInt += 1
+}
