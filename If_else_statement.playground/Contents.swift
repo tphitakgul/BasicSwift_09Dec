2938ed69b7c57e3432f88dd982d6dd432943d508
Boss <t.phitakgul@gmail.com>
2017-12-10 10:28:06 +0700

    if else statement

---
+++ If_else_statement.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let ageInt = 40
+
+if(ageInt < 18){
+    print("You are young  \(ageInt)")
+}else{
+    print("You are Old \(ageInt) ")
+}
+
+let nameString = "Doramon"
+let friendStrings = ["Doramon","Nopita"]
+
+if (nameString == friendStrings[0]) {
+    print("My friend \(nameString)")
+}else{
+    print("Not my friend")
+}
+
+
