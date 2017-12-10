13d90c9761baf31ee75928525858994e01b8cd90
Boss <t.phitakgul@gmail.com>
2017-12-10 14:06:23 +0700

    class and function ok

--- ClassAndFunction.playground/Contents.swift
+++ ClassAndFunction.playground/Contents.swift
@@ -9,7 +9,23 @@ class MyClass {
     var numberInt = 123
     var nameString = "Doramon"
     
+    //Create function void type
+    func myFunctionTypeVoid() -> Void {
+        print("This is Void function")
+    }
     
+    //Create function void and parameter
+    func myFunctionTypeVoidHaveArguement(surname:String) -> Void {
+        print("This is Void Parameter : \(surname)" )
+    }
+    func myFunctionTypeVoidHaveArguementMuti(para1Int:Int,para2Str:String) -> Void {
+        print("This is Void Parameter para1Int : \(para1Int) - para2Str \(para2Str)" )
+    }
+    
+    //Create Function Return Type
+    func calculateArea(base:Double,height:Double) -> Double {
+        return 0.5 * base * height
+    }
     
 }//end myclass
 
@@ -28,3 +44,12 @@ myClass.numberInt = 1000;
 //not getter setter
 print("after Number \(myClass.numberInt)")
 
+//How to call function of MyClass
+myClass.myFunctionTypeVoid()
+
+myClass.myFunctionTypeVoidHaveArguement(surname:"dd")
+
+myClass.myFunctionTypeVoidHaveArguementMuti(para1Int:123,para2Str:"test")
+
+var area = myClass.calculateArea(base: 5.4, height: 2.3)
+print("area -> \(area) ")
